76d4dc02964c8d80b8aa5e14636a5dd1c0015522
nicolasjakob <nicow.jakob@gmail.com>
2016-09-24 23:30:16 +0200

    Use arrow functions in tests

--- test/error.test.js
+++ test/error.test.js
@@ -2,17 +2,17 @@
 import { expect } from 'chai';
 import { UngrantedScopeError } from '../lib';
 
-describe('test/error.test.js', function () {
-  describe('with missing scopes parameter', function () {
-    it('should be an instance of Error', function () {
+describe('test/error.test.js', () => {
+  describe('with missing scopes parameter', () => {
+    it('should be an instance of Error', () => {
       expect(new UngrantedScopeError(['email']) instanceof Error).to.be.equal(true);
     });
-    it('should be an instance of UngrantedScopeError', function () {
+    it('should be an instance of UngrantedScopeError', () => {
       console.log(new UngrantedScopeError() instanceof UngrantedScopeError);
 
       expect(new UngrantedScopeError(['email']) instanceof UngrantedScopeError).to.be.equal(true);
     });
-    it('should have generic Error properties', function () {
+    it('should have generic Error properties', () => {
       expect(new UngrantedScopeError(['email'])).to.have.property('name').and.to.be.equal('UngrantedScopeError');
       expect(new UngrantedScopeError(['email'])).to.have.property('message');
       expect(new UngrantedScopeError(['email'])).to.have.property('stack');
--- test/scope-test.test.js
+++ test/scope-test.test.js
@@ -2,52 +2,52 @@
 import { expect } from 'chai';
 import scopeTest, { UngrantedScopeError } from '../lib';
 
-describe('test/scope-test.test.js', function () {
-  describe('with no granted scopes', function () {
-    it('should not throw an error', function () {
-      expect(function () {
+describe('test/scope-test.test.js', () => {
+  describe('with no granted scopes', () => {
+    it('should not throw an error', () => {
+      expect(() => {
         scopeTest([]);
       }).to.not.throw(Error);
     });
   });
-  describe('with no granted scopes and with no required scopes', function () {
-    it('should not throw an error', function () {
-      expect(function () {
+  describe('with no granted scopes and with no required scopes', () => {
+    it('should not throw an error', () => {
+      expect(() => {
         scopeTest([], { requiredScopes: [] });
       }).to.not.throw(Error);
     });
   });
-  describe('with missing granted scopes', function () {
-    it('should throw an ungranted error with missing scopes', function () {
-      expect(function () {
+  describe('with missing granted scopes', () => {
+    it('should throw an ungranted error with missing scopes', () => {
+      expect(() => {
         scopeTest([], { requiredScopes: ['email'] });
       }).to.throw(UngrantedScopeError).and.to.have.property('missingScopes').and.deep.equal(['email']);
     });
   });
-  describe('with matching granted scopes', function () {
-    it('should not throw an error', function () {
-      expect(function () {
+  describe('with matching granted scopes', () => {
+    it('should not throw an error', () => {
+      expect(() => {
         scopeTest(['email'], { requiredScopes: ['email'] });
       }).to.not.throw(Error);
     });
   });
-  describe('with matching granted scopes and extra granted scopes', function () {
-    it('should not throw an error', function () {
-      expect(function () {
+  describe('with matching granted scopes and extra granted scopes', () => {
+    it('should not throw an error', () => {
+      expect(() => {
         scopeTest(['email', 'profile'], { requiredScopes: ['email'] });
       }).to.not.throw(Error);
     });
   });
-  describe('with case insensitive matching granted scopes and with case sensitive option', function () {
-    it('should not throw an error', function () {
-      expect(function () {
+  describe('with case insensitive matching granted scopes and with case sensitive option', () => {
+    it('should not throw an error', () => {
+      expect(() => {
         scopeTest(['Email'], { requiredScopes: ['email'], ignoreCase: false });
       }).to.throw(UngrantedScopeError);
     });
   });
-  describe('with case insensitive matching granted scopes and with case insensitive option', function () {
-    it('should not throw an error', function () {
-      expect(function () {
+  describe('with case insensitive matching granted scopes and with case insensitive option', () => {
+    it('should not throw an error', () => {
+      expect(() => {
         scopeTest(['Email'], { requiredScopes: ['email'], ignoreCase: true });
       }).to.not.throw(Error);
     });
