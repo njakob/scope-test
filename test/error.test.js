19ce32bcc63bf4d3f6836128e06681013f216bc1
nicolasjakob <nicow.jakob@gmail.com>
2016-09-24 23:19:01 +0200

    Refactor tests with es6 and flowtype

--- test/error.test.js
+++ test/error.test.js
@@ -1,19 +1,21 @@
-"use strict";
 
-var UngrantedScopeError = require('../lib/').UngrantedScopeError;
+import { expect } from 'chai';
+import { UngrantedScopeError } from '../lib';
 
-describe('test/error.test.js', function() {
-  describe('with missing scopes parameter', function() {
-    it('should be an instance of Error', function() {
-      expect(new UngrantedScopeError([ 'email' ]) instanceof Error).to.be.equal(true);
+describe('test/error.test.js', function () {
+  describe('with missing scopes parameter', function () {
+    it('should be an instance of Error', function () {
+      expect(new UngrantedScopeError(['email']) instanceof Error).to.be.equal(true);
     });
-    it('should be an instance of UngrantedScopeError', function() {
-      expect(new UngrantedScopeError([ 'email' ]) instanceof UngrantedScopeError).to.be.equal(true);
+    it('should be an instance of UngrantedScopeError', function () {
+      console.log(new UngrantedScopeError() instanceof UngrantedScopeError);
+
+      expect(new UngrantedScopeError(['email']) instanceof UngrantedScopeError).to.be.equal(true);
     });
-    it('should have generic Error properties', function() {
-      expect(new UngrantedScopeError([ 'email' ])).to.have.property('name').and.to.be.equal('UngrantedScopeError');
-      expect(new UngrantedScopeError([ 'email' ])).to.have.property('message');
-      expect(new UngrantedScopeError([ 'email' ])).to.have.property('stack');
+    it('should have generic Error properties', function () {
+      expect(new UngrantedScopeError(['email'])).to.have.property('name').and.to.be.equal('UngrantedScopeError');
+      expect(new UngrantedScopeError(['email'])).to.have.property('message');
+      expect(new UngrantedScopeError(['email'])).to.have.property('stack');
     });
   });
 });
--- test/helpers/chai.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var chai = require('chai');
-
-global.expect = chai.expect;
-global.AssertionError = chai.AssertionError;
-global.Assertion = chai.Assertion;
-global.assert = chai.assert;
